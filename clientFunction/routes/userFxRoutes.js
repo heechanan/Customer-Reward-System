190a1089f321f319b1bcd16960ddd5e007d9d3df
heechanan <heechanan22@gmail.com>
2023-03-21 21:32:08 +0900

    [Update] ECS error check - Target group unhealthy

--- clientFunction/routes/userFxRoutes.js
+++ clientFunction/routes/userFxRoutes.js
@@ -6,4 +6,6 @@ module.exports = (fastify) => {
     fastify.post('/user/attendance2', userFxController.UserAttendanceList);  // 사용자 본인 출석 내역 조회
 
     fastify.get('/user/reward', userFxController.reward);  // 리워드 조회
+
+    fastify.get('/', userFxController.check);
 }
