3e778dc9075a7fbd6a5eceac7c91fd6dd4dc14d6
younggyeol <vmflr2@gmail.com>
2023-03-21 13:30:44 +0900

    [Fix] Change source code to read event values

--- StockIncreaseLambda/index.js
+++ StockIncreaseLambda/index.js
@@ -6,26 +6,29 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context) => {
     const message_body = JSON.stringify(event.Records[0].body);
+    const body = JSON.parse(message_body)
+    const Body = JSON.parse(body)
 
-    console.log("event body: ", message_body);
-    console.log("전송시간: ", now.toLocaleString("ko-kr", { timeZone: "Asia/Seoul" }));
+    console.log("event: ", Body);
+
     // 리워드 카운트가 3개 이하일때
-    if (message_body.rewardCount <= 3) {
+    if (Body.rewardCount <= 3) {
       const params = {
         TableName: 'rewardInfoTable',
         Key: {
-          'rewardDay': message_body.rewardDay
+          'rewardDay': Body.rewardDay
         },
         UpdateExpression: 'ADD rewardCount :val',
         ExpressionAttributeValues: {
           ':val': 10
         }
       };
+      console.log("전송시간: ", now.toLocaleString("ko-kr", { timeZone: "Asia/Seoul" }));
 
       try {
         await dynamoDB.update(params).promise();
         /// 재고 증가 완료 메시지
-        const message = `${now.toLocaleString("ko-kr", { timeZone: "Asia/Seoul" })} rewardDay ${message_body.rewardDay} 의 ${message_body.rewardName} 재고를 ${params.ExpressionAttributeValues[':val']} 개 증가하였습니다.`
+        const message = `${now.toLocaleString("ko-kr", { timeZone: "Asia/Seoul" })} rewardDay ${Body.rewardDay} 의 ${Body.rewardName} 재고를 ${params.ExpressionAttributeValues[':val']} 개 증가하였습니다.`
         console.log("전송 message: ", message);
         
         ///SNS 메세지 전송
